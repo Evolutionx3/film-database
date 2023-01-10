5febbf72cfb252187680bb4211cf4a6185b98a84
Evolutionx3 <frontdav1707@gmail.com>
2023-01-11 00:19:12 +0100

    Basic config for project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import "./App.scss";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route index element={<h1>Hello World</h1>} />
+          <Route path="movie/:id" element={<h1>Movie detail page</h1>} />
+          <Route path="movies/:type" element={<h1>Movies list page</h1>} />
+          <Route path="/*" element={<h1>Error page</h1>} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
